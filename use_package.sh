8547dd88e621dfb15563247b60432a3d6ff85b49
ZReKoJ <zhrekoj@gmail.com>
2025-02-23 20:55:30 +0100

    PATH_VARIABLE_MODE

--- use_package.sh
+++ use_package.sh
@@ -12,11 +12,26 @@ help() {
 	echo "    regex     : Optimize the selection for less results, regex matching, can be null, the result will be added to the config file, if for the same package with different version was found, will be replaced"
 }
 
+get_config_value() {
+	local param=$1
+	local mode_value
+	mode_value=$(grep -Po "^$1=\K.*" "$MODE_FILE")
+	if [[ -n "$mode_value" ]]; then
+        echo "$mode_value"
+		exit 0
+    else
+        echo "Param $param not found in $MODE_FILE"
+		exit 1
+    fi
+}
+
 # Define variables
 
 CHECK_PATH="$HOME/packages/"
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+LINK_DIR="$SCRIPT_DIR/links"
 CONFIG_FILE="$SCRIPT_DIR/.default.cfg"
+MODE_FILE="$SCRIPT_DIR/.mode.cfg"
 POSITIONAL_ARGS=()
 
 # Parse arguments
@@ -54,6 +69,24 @@ set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
 # Checkings
 
+if [ ! -f "$MODE_FILE" ]; then
+	cat << EOF > $MODE_FILE
+# PATH_VARIABLE_MODE: by setting all paths to the \$PATH variable"
+# SYMBOLIC_LINK_MODE: with symbolic link
+MODE=PATH_VARIABLE_MODE
+EOF
+fi
+
+MODE=$(get_config_value "MODE")
+if [ $? -eq 1 ]; then
+	echo $MODE
+	exit 1
+fi
+
+if [ ! -d "$LINK_DIR" ]; then
+    mkdir "$LINK_DIR"
+fi
+
 # Execution
 
 cd $CHECK_PATH
@@ -68,17 +101,26 @@ do
 		then
 			type_package=`echo $package | cut -d "/" -f2`
 			bin_path=`find $package -iname "bin" -type d | awk '(NR == 1 || length < length(shortest)) { shortest = $0 } END { print shortest }'`
+			# In case there is no bin folder, the folder is where the bins are located
+			if [ -z "$variable" ]; then
+				bin_path=$package
+			fi 
+
 			if [ -d $bin_path ];
 			then
 				cd $bin_path
+				
+				case $MODE in 
+					"PATH_VARIABLE_MODE")
+						sed -i "\|${CHECK_PATH}${type_package}|d" $CONFIG_FILE
+						echo "$PWD" >> $CONFIG_FILE
 
-				sed -i "\|${CHECK_PATH}${type_package}|d" $CONFIG_FILE
-				echo "$PWD" >> $CONFIG_FILE
-
-				echo ""
-				echo "New Config file:"
-				echo ""
-				cat $CONFIG_FILE
+						echo ""
+						echo "New Config file:"
+						echo ""
+						cat $CONFIG_FILE
+					;;
+				esac
 			fi	
 			
 			break
